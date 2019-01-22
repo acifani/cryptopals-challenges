2481cb79fc18bc9ca341245f241c87bff949b551
Alessandro Cifani <alessandro.cifani@gmail.com>
2019-01-22 21:46:57 +0100

    Fix problem 02

--- set1/02_test.go
+++ set1/02_test.go
@@ -1,18 +1,29 @@
 package set1
 
 import (
+	"encoding/hex"
 	"bytes"
 	"testing"
 )
 
-func TextFixedXOR(t *testing.T) {
-	inputA := []byte("1c0111001f010100061a024b53535009181c")
-	inputB := []byte("686974207468652062756c6c277320657965")
-	expected := []byte("746865206b696420646f6e277420706c6179")
+func TestFixedXOR(t *testing.T) {
+	inputA := hexDecode([]byte("1c0111001f010100061a024b53535009181c"), t)
+	inputB := hexDecode([]byte("686974207468652062756c6c277320657965"), t)
+	expected := hexDecode([]byte("746865206b696420646f6e277420706c6179"), t)
 
 	output, _ := FixedXOR(inputA, inputB)
 
 	if !bytes.Equal(expected, output) {
 		t.Fatalf("Expected %v, but got %v", expected, output)
 	}
+}
+
+func hexDecode(input []byte, t *testing.T) []byte {
+	output := make([]byte, hex.DecodedLen(len(input)))
+	_, err := hex.Decode(output, input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return output
 }
