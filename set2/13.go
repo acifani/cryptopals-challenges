cbac9080617e4e79ecf2e614a125a6a64d68c7b7
Alessandro Cifani <alessandro.cifani@gmail.com>
2019-03-27 18:59:43 +0100

    Challenge 13

--- set2/13.go
+++ set2/13.go
@@ -45,8 +45,10 @@ and the ciphertexts themselves, make a role=admin profile.
 package set2
 
 import (
+	"log"
 	"math/rand"
 	"net/url"
+	"strconv"
 
 	"github.com/acifani/cryptopals-challenges/set1"
 )
@@ -54,24 +56,25 @@ import (
 func profileFor(email string) string {
 	values := url.Values{}
 	values.Set("email", email)
-	values.Set("uid", string(rand.Intn(1000)))
+	values.Set("uid", strconv.Itoa(rand.Intn(99)))
 	values.Set("role", "user")
 	return values.Encode()
 }
 
-func cookieECBOracle() (encrypt func(email string) string, decrypt func(cookie string) url.Values) {
+func cookieECBOracle() (encrypt func(email []byte) []byte, decrypt func(cookie []byte) url.Values) {
 	key := make([]byte, 16)
 	rand.Read(key)
 
-	encrypt = func(email string) string {
-		profile := profileFor(email)
+	encrypt = func(email []byte) []byte {
+		profile := profileFor(string(email))
 		paddedProfile := PadRight([]byte(profile), 16)
 		encrypted := EncryptAESinECB(paddedProfile, key)
-		return string(encrypted)
+		return encrypted
 	}
 
-	decrypt = func(cookie string) url.Values {
+	decrypt = func(cookie []byte) url.Values {
 		decrypted := set1.DecryptAESinECB([]byte(cookie), key)
+		log.Printf("\n%s", decrypted)
 		values, err := url.ParseQuery(string(decrypted))
 		if err != nil {
 			panic(err)
@@ -82,6 +85,23 @@ func cookieECBOracle() (encrypt func(email string) string, decrypt func(cookie s
 	return encrypt, decrypt
 }
 
-func BreakCookieECBORacle() {
+// BreakECBCookie given an encrypt cookie oracle that generates
+// cookie profile in the format: `email=foo@bar.me&role=user&uid=99<padding>`,
+// this function will forge an encrypted admin cookie.
+func BreakECBCookie(encrypt func(email []byte) []byte) []byte {
+	blockSize := 16
+	// cookie = "email=foo@bar.me | &role=user&uid=9 | 9<padding>"
 
+	// Block 1: email=foo@bar.me
+	block1 := encrypt([]byte("foo@bar.me"))[:blockSize]
+	// Block 2: 1234567890&role=
+	block2 := encrypt([]byte("12345678901234567890"))[blockSize : blockSize*2]
+	// Block 3: admin&role=user&uid=99<padding>
+	block3 := encrypt([]byte("1234567890admin"))[blockSize:]
+
+	// We have to concat 2 different 'role' keys because of the way
+	// Go's url module orders the value keys in the dictionary.
+	// It would've been much easier otherwise, but let's pretend we can't.
+	// email=foo@bar.me1234567890&role=admin&role=user&uid=99<padding>
+	return append(append(block1, block2...), block3...)
 }
--- set2/13_test.go
+++ set2/13_test.go
@@ -13,19 +13,34 @@ func TestProfileFor(t *testing.T) {
 		t.Fatal(err)
 	}
 	if output.Get("email") != email {
-		t.Fatalf("Expected %v but got %v", email, output.Get("email"))
+		t.Fatalf("Expected '%v' but got '%v'", email, output.Get("email"))
 	}
 	if output.Get("role") != "user" {
-		t.Fatalf("Expected 'user' but got %v", output.Get("role"))
+		t.Fatalf("Expected 'user' but got '%v'", output.Get("role"))
 	}
 }
 
 func TestCookieECBOracle(t *testing.T) {
-	input := "foo@bar.com"
+	input := []byte("foo@bar.com")
 
 	encrypt, decrypt := cookieECBOracle()
 	output := decrypt(encrypt(input))
-	if output.Get("email") != input {
-		t.Fatalf("Expected %v but got %v", input, output.Get("email"))
+
+	if output.Get("email") != string(input) {
+		t.Fatalf("Expected '%v' but got '%v'", input, output.Get("email"))
+	}
+
+	if output.Get("role") != "user" {
+		t.Fatalf("Expected 'user' but got '%v'", output.Get("role"))
+	}
+}
+
+func TestBreakECBCookie(t *testing.T) {
+	encrypt, decrypt := cookieECBOracle()
+	output := BreakECBCookie(encrypt)
+
+	values := decrypt(output)
+	if values.Get("role") != "admin" {
+		t.Fatalf("Expected 'admin' but got '%v'", values.Get("role"))
 	}
 }
